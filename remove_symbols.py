6491698e26fa5796c0725a65a8c9a95a389e2204
iakovleva <paulina.iakovleva@gmail.com>
2017-12-13 11:19:36 +0200

    Add script for removing punctuation

---
+++ remove_symbols.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python
+
+import sys
+import re
+
+symbol_list = [
+    '\?', '!', '@', '·', '#', '\$', '\%', '\^', '\&', '\*', '\(', '\)', '\[', '\]',
+    '\{', '\}', '\/', '\|', r'\\', '№', '\+', '<', '>', ':', ';', '“', '“', '`', '~', '=',
+]
+
+
+def main():
+    try:
+        if len(sys.argv) > 2:
+            print ("Please enter only 1 text string query")
+        else:
+            query = sys.argv[1]
+            # remove '_' in any case except spu_orb 
+            if 'spu_orb' not in query:
+                query = re.sub(r'_', ' ', query)
+            # replace comma with space
+            if ',' in query:
+                query = re.sub(r',', ' ', query)
+            # remove other punctuation
+            for symbol in symbol_list:
+                result = re.search(symbol, query)
+                if result:
+                    query = re.sub(symbol, ' ', query)
+            # remove spaces, dots and dashes in the end and at the beginning
+            query = query.strip(' -.')
+            # remove double spaces
+            query = re.sub("\s\s+", " ", query)
+            print (query, len(query))
+    except IndexError:
+        print ("Enter a query")
+
+if __name__ == '__main__':
+    main()
