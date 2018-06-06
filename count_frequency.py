d8bc1d4f638cc73aea25607fef3305041b124d30
iakovleva <paulina.iakovleva@gmail.com>
2018-06-06 10:25:22 +0300

    Copy all columns from Sheet1 to Sheet2 when counting frequent words. Check for empty rows'

--- count_frequency.py
+++ count_frequency.py
@@ -12,18 +12,36 @@ def count_frequency(fl):
 
     # Create Counter for all words in column
     for i in range(1, sh.max_row):
-            key = sh.cell(row=i, column=1).value
-            for word in key.split():
-                cnt[word] += 1
+            cell =  sh.cell(row=i, column=1).value
+            if cell is not None:
+                for word in cell.split():
+                    cnt[word] += 1
 
     new_row_start = 1
     for k, v in cnt.most_common(40):
         print(k)
         for i in range(1, sh.max_row):
-            if k in sh.cell(row=i, column=1).value.split():
-                sh2.cell(row=new_row_start, column=1).value = sh.cell(row=i,column=1).value
-                sh2.cell(row=new_row_start, column=2).value = k
-                new_row_start += 1
+            cell =  sh.cell(row=i, column=1).value
+            if cell is not None:
+                if k in cell.split():
+                    sh2.cell(row=new_row_start, column=1).value = sh.cell(row=i,column=1).value
+                    sh2.cell(row=new_row_start, column=2).value = k
+                    sh2.cell(row=new_row_start, column=3).value = sh.cell(row=i,column=2).value
+                    sh2.cell(row=new_row_start, column=4).value = sh.cell(row=i,column=3).value
+                    sh2.cell(row=new_row_start, column=5).value = sh.cell(row=i,column=4).value
+                    sh2.cell(row=new_row_start, column=6).value = sh.cell(row=i,column=5).value
+                    sh2.cell(row=new_row_start, column=7).value = sh.cell(row=i,column=6).value
+                    sh2.cell(row=new_row_start, column=8).value = sh.cell(row=i,column=7).value
+                    sh2.cell(row=new_row_start, column=9).value = sh.cell(row=i,column=8).value
+                    sh2.cell(row=new_row_start, column=10).value = sh.cell(row=i,column=9).value
+                    sh2.cell(row=new_row_start, column=11).value = sh.cell(row=i,column=10).value
+                    sh2.cell(row=new_row_start, column=12).value = sh.cell(row=i,column=11).value
+                    sh2.cell(row=new_row_start, column=13).value = sh.cell(row=i,column=12).value
+                    sh2.cell(row=new_row_start, column=14).value = sh.cell(row=i,column=13).value
+                    sh2.cell(row=new_row_start, column=15).value = sh.cell(row=i,column=14).value
+                    sh2.cell(row=new_row_start, column=16).value = sh.cell(row=i,column=15).value
+                    sh2.cell(row=new_row_start, column=17).value = sh.cell(row=i,column=16).value
+                    new_row_start += 1
     wb.save(fl)
 
 if __name__ == '__main__':
