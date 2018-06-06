3089e1e11e2278501952ac97f52e0533eb4b21c9
iakovleva <paulina.iakovleva@gmail.com>
2018-06-06 11:06:38 +0300

    Add headers row copied from Sheet1 in a new sheet

--- count_frequency.py
+++ count_frequency.py
@@ -7,40 +7,48 @@ from collections import Counter
 def count_frequency(fl):
     wb = openpyxl.load_workbook(fl)
     sh = wb.active
-    sh2 = wb.create_sheet(title='Sheet2')
     cnt = Counter()
+
+    # Create new sheet with headers row
+    sh2 = wb.create_sheet(title='Sheet2')
+    sh2.cell(row=1, column=1).value = sh.cell(row=1, column=1).value
+    sh2.cell(row=1, column=2).value = 'Word'
+    for col in range(3, 18):
+        sh2.cell(row=1, column=col).value = sh.cell(row=1, column=col-1).value
 
     # Create Counter for all words in column
     for i in range(1, sh.max_row):
-            cell =  sh.cell(row=i, column=1).value
+            cell = sh.cell(row=i, column=1).value
             if cell is not None:
                 for word in cell.split():
                     cnt[word] += 1
 
-    new_row_start = 1
+    # Check if each common word is in each keyword phrase, create new row with all
+    # data about kw plus a column with commom word in this phrase
+    new_row_start = 2
     for k, v in cnt.most_common(40):
         print(k)
         for i in range(1, sh.max_row):
-            cell =  sh.cell(row=i, column=1).value
+            cell = sh.cell(row=i, column=1).value
             if cell is not None:
                 if k in cell.split():
-                    sh2.cell(row=new_row_start, column=1).value = sh.cell(row=i,column=1).value
+                    sh2.cell(row=new_row_start, column=1).value = sh.cell(row=i, column=1).value
                     sh2.cell(row=new_row_start, column=2).value = k
-                    sh2.cell(row=new_row_start, column=3).value = sh.cell(row=i,column=2).value
-                    sh2.cell(row=new_row_start, column=4).value = sh.cell(row=i,column=3).value
-                    sh2.cell(row=new_row_start, column=5).value = sh.cell(row=i,column=4).value
-                    sh2.cell(row=new_row_start, column=6).value = sh.cell(row=i,column=5).value
-                    sh2.cell(row=new_row_start, column=7).value = sh.cell(row=i,column=6).value
-                    sh2.cell(row=new_row_start, column=8).value = sh.cell(row=i,column=7).value
-                    sh2.cell(row=new_row_start, column=9).value = sh.cell(row=i,column=8).value
-                    sh2.cell(row=new_row_start, column=10).value = sh.cell(row=i,column=9).value
-                    sh2.cell(row=new_row_start, column=11).value = sh.cell(row=i,column=10).value
-                    sh2.cell(row=new_row_start, column=12).value = sh.cell(row=i,column=11).value
-                    sh2.cell(row=new_row_start, column=13).value = sh.cell(row=i,column=12).value
-                    sh2.cell(row=new_row_start, column=14).value = sh.cell(row=i,column=13).value
-                    sh2.cell(row=new_row_start, column=15).value = sh.cell(row=i,column=14).value
-                    sh2.cell(row=new_row_start, column=16).value = sh.cell(row=i,column=15).value
-                    sh2.cell(row=new_row_start, column=17).value = sh.cell(row=i,column=16).value
+                    sh2.cell(row=new_row_start, column=3).value = sh.cell(row=i, column=2).value
+                    sh2.cell(row=new_row_start, column=4).value = sh.cell(row=i, column=3).value
+                    sh2.cell(row=new_row_start, column=5).value = sh.cell(row=i, column=4).value
+                    sh2.cell(row=new_row_start, column=6).value = sh.cell(row=i, column=5).value
+                    sh2.cell(row=new_row_start, column=7).value = sh.cell(row=i, column=6).value
+                    sh2.cell(row=new_row_start, column=8).value = sh.cell(row=i, column=7).value
+                    sh2.cell(row=new_row_start, column=9).value = sh.cell(row=i, column=8).value
+                    sh2.cell(row=new_row_start, column=10).value = sh.cell(row=i, column=9).value
+                    sh2.cell(row=new_row_start, column=11).value = sh.cell(row=i, column=10).value
+                    sh2.cell(row=new_row_start, column=12).value = sh.cell(row=i, column=11).value
+                    sh2.cell(row=new_row_start, column=13).value = sh.cell(row=i, column=12).value
+                    sh2.cell(row=new_row_start, column=14).value = sh.cell(row=i, column=13).value
+                    sh2.cell(row=new_row_start, column=15).value = sh.cell(row=i, column=14).value
+                    sh2.cell(row=new_row_start, column=16).value = sh.cell(row=i, column=15).value
+                    sh2.cell(row=new_row_start, column=17).value = sh.cell(row=i, column=16).value
                     new_row_start += 1
     wb.save(fl)
 
